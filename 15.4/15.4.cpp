c9094e2d6dd43123c3ee23677495a8dcc4b25136
Ian <80196814+Screll3@users.noreply.github.com>
2022-07-01 14:45:50 +0700

    HomeWork
    V 1.0

---
+++ 15.4/15.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+void chetprinter(int n, bool chet)
+{
+    if (chet)
+        for (int i = 0; i <= n; i += 2)
+            std::cout << i << " ";
+    else
+        for (int i = 1; i <= n; i += 2)
+            std::cout << i << " ";
+}
+
+int main()
+{
+    const int n = 13;
+    std::cout << "Using 'for'\n";
+    for (int i = 0; i <= n; i+=2)
+        std::cout << i << " ";
+    std::cout << "\nUsing functions\n";
+    chetprinter(n, true);
+    std::cout << "\n";
+    chetprinter(n, false);
+}
